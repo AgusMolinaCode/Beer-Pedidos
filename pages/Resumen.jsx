c6f03fcca372da6ba5ee8b8883c52ec21d868a5d
Agustin Molina <agustinmolina@MacBook-Air-de-Agustin.local>
2023-05-31 18:28:48 -0300

    2.1

--- pages/Resumen.jsx
+++ pages/Resumen.jsx
@@ -14,7 +14,7 @@ export default function Resumen() {
                     <p className="font-principal text-[2rem] font-semibold text-center text-black">Resumen:</p>
                 </div>
 
-                <div className="pb-20">
+                <div className="pb-20 px-2">
                     {
                         pedido.map(producto => (
                             <ResumenProducto key={producto.id} producto={producto}/>

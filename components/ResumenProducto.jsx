504ee513f995e34d79a9a9b73c3f4a9a3d882675
Agustin Molina <agustinmolina@MacBook-Air-de-Agustin.local>
2023-05-31 18:24:26 -0300

    2.0

--- components/ResumenProducto.jsx
+++ components/ResumenProducto.jsx
@@ -11,28 +11,28 @@ export const ResumenProducto = ({producto}) => {
 
   return (
     <div>
-        <div className='flex gap-5 justify-center items-center'>
+        <div className='flex flex-wrap mx-auto justify-center items-center border-2 border-gray-400 rounded-xl mt-5 p-2'>
             
-            <Image className={`${categoriaActual?.id === 1 ? 'w-[150px]' : 'w-[150px]'} `} src={`/assets/images/${producto.imagen}.png`} alt="imagen producto" width="150" height="150" />
+            <Image className='w-[80px] md:[150px]'  src={`/assets/images/${producto.imagen}.png`} alt="imagen producto" width="150" height="150" />
 
-            <div className='flex-col items-center w-[450px] justify-center'>
-              <h1 className='font-principal text-5xl'>{producto.nombre}</h1>
-              <p className='font-principal text-2xl mt-5'>Cantidad: {producto.cantidad}</p>
-              <p className='font-principal text-4xl mt-2 text-indigo-600'>Precio ${producto.precio}</p>
-              <p className='font-principal text-2xl mt-2'>SubTotal: ${subtotal}</p>
+            <div className='flex-col items-center mx-auto md:w-[450px] justify-center'>
+              <h1 className='font-principal text-[1.55rem] md:text-5xl'>{producto.nombre}</h1>
+              <p className='font-principal  text-xl md:text-2xl mt-5'>Cantidad: {producto.cantidad}</p>
+              <p className='font-principal text-3xl md:text-4xl mt-2 text-indigo-600'>Precio ${producto.precio}</p>
+              <p className='font-principal text-xl md:text-2xl mt-2'>SubTotal: ${subtotal}</p>
             </div>
 
-            <div>
+            <div className=''>
 
               <button 
                 onClick={() => handleEditarCantidad(producto.id)}
-                className='text-2xl font-principal text-indigo-500 flex gap-2'>
+                className='md:text-2xl text-lg font-principal text-indigo-500 flex gap-2'>
                 <BsXCircle size={32} /> Editar
               </button>
 
               <button 
                 onClick={() => handleEliminarProducto(producto.id)}
-                className='mt-4 text-2xl font-principal text-red-500 flex gap-2'>
+                className='mt-4 text-lg md:text-2xl font-principal text-red-500 flex gap-2'>
                 <BsXCircle size={32} /> Eliminar
               </button>
 
